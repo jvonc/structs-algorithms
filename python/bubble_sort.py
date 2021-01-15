dde3740c725abd32affa9390ea2f7f36235e3281
jvonc <jvonmika@ualberta.ca>
2021-01-15 08:10:43 -0700

    Python folder

---
+++ python/bubble_sort.py
@@ -0,0 +1,5 @@
+def bubbleSort(array):
+    for i in range(len(array) - 1):
+        for j in range(len(array) - i - 1):
+            if array[j + 1] < array[j]:
+                array[j + 1], array[j] = array[j], array[j + 1]
